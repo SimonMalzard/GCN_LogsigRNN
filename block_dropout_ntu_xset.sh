a359e46d97cb7ebe31416cdefe1c79d63cac7ba6
SimonMalzard <smalzard@turing.ac.uk>
2025-09-30 16:07:00 +0100

    fixed incorrectly named files

--- block_dropout_ntu_xset.sh
+++ block_dropout_ntu_xset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # drop values for random dropout using dropout rates of 0% to 90% in increments of 10%. 
-drop_values=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) 
+resolution_values=(3 5 10 15 30) 
 
-config_file="/datadrive/GCN_LogsigRNN/config/ntu_sub/test_joint_xsub.yaml"
-work_dir="/datadrive/GCN_LogsigRNN/work_dir/ntu120/xsub/gcn_logsigrnn"
+config_file="/datadrive/GCN_LogsigRNN/config/ntu_sub/test_joint_xset.yaml"
+work_dir="/datadrive/GCN_LogsigRNN/work_dir/ntu120/xset/gcn_logsigrnn"
 
 echo "Current working directory: $(pwd)"
 
@@ -15,27 +15,27 @@ else
     echo "Config file exists at $config_file"
 fi
 
-for drop_value in "${drop_values[@]}"
+for resolution_value in "${resolution_values[@]}"
 do
-    echo "Running script with drop rate: $drop_value"
-    # Update the YAML config file with the new drop rate value using sed
-    sed -i "s/drop_rate: [0-9]*\.[0-9]\+/drop_rate: $drop_value/" "$config_file"
+    echo "Running script with structured_res: $resolution_value"
+    # Update the YAML config file with the new structured_res value using sed
+    sed -i "s/structured_res: [0-9]*/structured_res: $resolution_value/" "$config_file"
 
     # Check if sed updated the file correctly
-    updated_value=$(grep 'drop_rate' "$config_file" | awk '{print $2}')
-    if [ "$updated_value" != "$drop_value" ]; then
-        echo "Error: Failed to update drop_rate to $drop_value in $config_file"
+    updated_value=$(grep 'structured_res' "$config_file" | awk '{print $2}')
+    if [ "$updated_value" != "$resolution_value" ]; then
+        echo "Error: Failed to update structured_res to $resolution_value in $config_file"
         exit 1
     else
-        echo "Successfully updated drop_rate to $drop_value in $config_file"
+        echo "Successfully updated structured_res to $resolution_value in $config_file"
     fi
     
     # Print the updated config file content
     echo "Updated config file content:"
-    grep 'drop_rate' "$config_file"
+    grep 'structured_res' "$config_file"
 
     # Run the Python script with the specified command
-    python3 main.py --config ./config/ntu_sub/test_joint_xsub.yaml  --device 0 
-    echo "Finished running script with drop_rate: $drop_value"
+    python3 main.py --config ./config/ntu_sub/test_joint_xset.yaml  --device 0 
+    echo "Finished running script with structured_res: $resolution_value"
     echo "---------------------------------------------"
 done
